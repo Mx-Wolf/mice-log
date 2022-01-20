3cd2a5fad7fab5730cc511163e0dbb521db6657f
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-01-20 09:54:45 +0300

    initializing from process.env

--- src/imp/init-levels.ts
+++ src/imp/init-levels.ts
@@ -1,10 +1,23 @@
 import { LogInit } from './types';
-import { categories, severities } from './log';
+
+const MICE_LOG_KNOWN_PREFIX = 'mice_log_';
+
+const withSettings = (settings:NodeJS.ProcessEnv)=>(a:Map<string,string>, b:string)=>{
+  if(b.toLocaleLowerCase().startsWith(MICE_LOG_KNOWN_PREFIX) ){
+    const value = typeof settings[b];
+    if(typeof value === 'string')
+    {
+      a.set(b.substring(0,MICE_LOG_KNOWN_PREFIX.length).toLocaleLowerCase(), value);
+    }
+  }
+  return a;
+};
+
+const loadFrom = (settings:NodeJS.ProcessEnv)=>Object
+  .keys(settings)
+  .reduce(withSettings(settings), new Map<string,string>());
 
 export const getInitLevels = ():LogInit=>({
   basePath:'./',
-  levels: new Map([
-    [categories[0],severities[3]],
-    [categories[1],severities[3]],
-  ])
+  levels: loadFrom(process.env),
 });
--- src/imp/updater.ts
+++
@@ -1,3 +0,0 @@
-import { Category, Severity } from './log';
-
-export const withLevels = (levels:Map<Category,Severity>)=>(initLevels:Record<Category,Severity>)=> (category:Category) => levels.set(category, initLevels[category]);
